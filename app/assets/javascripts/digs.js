fb3da56b1c020e6b7cb2860fe05ba4014d55285b
Andy <andygnewman@gmail.com>
2015-03-25 10:28:02 +0000

    hiding Dig this post link once clicked as user not able to dig multiple times

--- app/assets/javascripts/digs.js
+++ app/assets/javascripts/digs.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
 
   $('.digs-link').on('click', function(event){
-      event.preventDefault();
+    event.preventDefault();
 
-      var digCount = $(this).siblings('.digs_count');
+    var digCount = $(this).siblings('.digs_count');
 
-      $.post(this.href, function(response){
-        digCount.text(response.new_dig_count);
+    $.post(this.href, function(response){
+      digCount.text(response.new_dig_count);
     })
+
+    $(this).hide();
+
   })
 })
