86568d9220f7840dd7f945e08709b102f5224db7
Andy <andygnewman@gmail.com>
2015-03-25 16:01:42 +0000

    further attempt to get s3 working with url and path in defaults

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,8 +85,8 @@ Rails.application.configure do
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     },
-    # :url =>':s3_domain_url'
-    # :path => '???'
+    :url =>':s3_domain_url',
+    :path => '/:class/:attachment/:id_partition/:style/:filename'
   }
 
 end
