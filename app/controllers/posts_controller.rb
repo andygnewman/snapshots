4d6a2c225b8fe9de482102f5f8922d991c0673a4
Andy <andygnewman@gmail.com>
2015-03-25 13:15:00 +0000

    styling of users own snapshots

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, :except => [:index, :show]
 
   def index
-    @user_posts = Post.where(user: current_user).order(created_at: :desc)
+    # @user_posts = Post.where(user: current_user).order(created_at: :desc)
     @posts = Post.order(created_at: :desc)
   end
 
