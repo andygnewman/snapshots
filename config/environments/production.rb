c5c2207e84b067361230032214c4908c73ad44ad
Andy <andygnewman@gmail.com>
2015-03-25 15:10:53 +0000

    add s3 domain url to paperclip defaults

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,8 @@ Rails.application.configure do
       :bucket => ENV['S3_BUCKET_NAME_SNAPSHOTS'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
+    },
+    :url =>':s3_domain_url'
   }
 
 end
